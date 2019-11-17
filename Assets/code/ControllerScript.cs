5640b5bbe670d79f3b464a80ec13f296d6c46757
Matheus López Vélez <matjavierlopez@hotmail.com>
2019-11-17 13:18:32 -0500

    Add files via upload

---
+++ Assets/code/ControllerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ * To do List:
+ * Make "To do list": DONE
+ * Add sphere subject DONE
+ * Add field DONE
+ * Add materials
+ * add physics/colliders DONE
+ * add obstacles DONE
+ * add effects on collision
+ * enjoy
+*/
+public class ControllerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
